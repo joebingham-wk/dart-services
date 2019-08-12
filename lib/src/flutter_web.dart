034d9f9e027d067d91fd6e1c3c1a3f0be8960cc3
Keal Jones <keal.jones@workiva.com>
2019-08-12 11:27:00 -0700

    add current list of workiva pub packages

--- lib/src/flutter_web.dart
+++ lib/src/flutter_web.dart
@@ -142,10 +142,207 @@ $_samplePackageName:lib/
 
   static const String _samplePackageName = 'dartpad_sample';
 
+  // Last collected on 8/12/19
+  static const List<String> workivaHostedPubPackages = [
+    "doc_plat_client",
+    "graph_ui",
+    "home",
+    "web_skin_dart",
+    "cerebral_ui",
+    "w_translate",
+    "dart_permissions_editor",
+    "graph_api",
+    "audit",
+    "w_viewer",
+    "copy_ui",
+    "sa_tools_toolbox",
+    "w_comments",
+    "w_annotations_api",
+    "xbrl_module",
+    "w_filing",
+    "w_attachments",
+    "wdesk_login",
+    "blob_storage",
+    "w_router",
+    "w_graph_client",
+    "w_attachments_client",
+    "xbrl_orchestrator_api",
+    "undo_redo",
+    "w_input_validation",
+    "react_tracing",
+    "w_session",
+    "drawing",
+    "w_oauth2",
+    "user_analytics",
+    "wdesk_sdk",
+    "w_link_properties_ui",
+    "wContent_frugal",
+    "data_modeler",
+    "comments_frugal",
+    "w_sox",
+    "w_history",
+    "focus",
+    "workflow_client",
+    "review_bar",
+    "app_intelligence",
+    "w_table",
+    "design_system",
+    "storybook",
+    "licensing_frugal",
+    "w_clipboard",
+    "speedparser",
+    "web_skin",
+    "filing_orchestrator_api",
+    "wf_js_document_viewer",
+    "workflow_frugal",
+    "history_frugal",
+    "admin_client",
+    "wdesk_sdk_builders",
+    "workspaces_components",
+    "tasker",
+    "permissions_editor",
+    "markup",
+    "w_editor_dev_tools",
+    "w_user_color_module",
+    "w_outline",
+    "ale_frugal",
+    "visjs",
+    "w_dashboard",
+    "graph_form_api",
+    "workflow_catalog_assets",
+    "w_filing_api",
+    "sdlc_analytics",
+    "messaging_sdk",
+    "xbrl_importer_api",
+    "w_project",
+    "docmodel_frugal",
+    "dataset_service_api",
+    "audit_api",
+    "fastpath_api",
+    "find_ui",
+    "w_virtual_components",
+    "snapshot",
+    "bigsky_rest_files",
+    "linking_sdk",
+    "home_frugal",
+    "xbrl_config_api",
+    "workspaces_api",
+    "dataset_service_ui",
+    "wdata_ui_utils",
+    "admin_frugal",
+    "linking_frugal",
+    "workspaces_frugal",
+    "rcal_api",
+    "xbrl2_server_api",
+    "w_test_tools",
+    "licensing_api",
+    "w_dashboard_frugal",
+    "frugal",
+    "forms_definitions_experience",
+    "sa_tools_data_selections",
+    "copy_frugal",
+    "file_services_sdk",
+    "grc_services_frugal",
+    "commitlog_frugal"
+    "proofread_frugal",
+    "highcharts",
+    "over_react_format",
+    "web_skin_docs",
+    "thrift",
+    "w_webdriver_utils",
+    "sa_tools_rollforward",
+    "w_context_menu",
+    "workiva_scripts",
+    "vessel",
+    "mock_messaging_service_sdk",
+    "w_office_online_frame",
+    "tasker_frugal",
+    "tour",
+    "sockjs_client",
+    "mockito_compat",
+    "wuri_sdk",
+    "support_viewer_frugal",
+    "skaardb",
+    "text_doc_client",
+    "charts",
+    "datatables",
+    "w_editor_properties",
+    "key_binder",
+    "iam_landing_page",
+    "designated_driver",
+    "truss",
+    "basictracer_dart",
+    "dart_version",
+    "test_invoker",
+    "semver_audit",
+    "eva_admin_frugal",
+    "bigsky_webdriver_utils",
+    "abide",
+    "bender",
+    "eva_frugal",
+    "sw_toolbox_dart",
+    "contract_creator",
+    "dart_medic",
+    "admiral_ui",
+    "mms_example_idl",
+    "idm_frugal",
+    "unscripted",
+    "browser_storage",
+    "notification_services_frugal",
+    "content_management_api_frugal",
+    "standards_api",
+    "opentracing_dart",
+    "data_platform_api_frugal",
+    "font_face_observer",
+    "content_search_frugal",
+    "fs_viewer_api",
+    "toolbars",
+    "lux_bindings",
+    "doc_client",
+    "content_search_service_frugal",
+    "support_api_frugal",
+    "lux_editor",
+    "w_chart",
+    "xbrl2_validation_api",
+    "w_crdt",
+    "resource_index_frugal",
+    "xbrl_translator_api",
+    "notifications_service_frugal",
+    "announcement_feed",
+    "wdata",
+    "filing_api",
+    "wdesk_examples",
+    "sauce_unit_test_runner",
+    "search_client",
+    "infer_client",
+    "ixbrl_importer_api",
+    "webdwiver",
+    "comments_and_tasks",
+    "wContent",
+    "color",
+    "licensing_admin_dart",
+    "graph_printing_orchestrator_sdk",
+    "licensing_api_dart",
+    "cerberus_dart",
+    "platform_detect",
+    "xbrl2_server_frontend",
+    "docsserver_dart",
+    "role_manager",
+    "w_common",
+    "shapes",
+    "workspaces",
+    "wlayout",
+    "dev_portal",
+    "dev_console_shell",
+    "rcal_client",
+    "wSoxDashboards",
+    "certifier",
+    "wchart"
+  ];
+
   // TODO: Add support for git overrides
   static String generateDependency(String package, [String version = 'any']) {
-    final workivaPub = ['web_skin', 'web_skin_dart'];
-    if (workivaPub.contains(package)){
+    if (workivaHostedPubPackages.contains(package)){
       return '''
   $package:
     hosted:
@@ -162,15 +359,12 @@ $_samplePackageName:lib/
 name: $_samplePackageName
 ''';
 
-if (packages?.isNotEmpty != null){
-  content += '\ndependencies:\n';
-  packages.forEach((package, version){
-    content += generateDependency(package, version);
-  });
-}
-
-
-
+    if (packages?.isNotEmpty != null){
+      content += '\ndependencies:\n';
+      packages.forEach((package, version){
+        content += generateDependency(package, version);
+      });
+    }
     if (includeFlutterWeb) {
       content += '''
 dev_dependencies:
@@ -181,14 +375,3 @@ dev_dependencies:
     return content;
   }
 }
-
-/*
-  react:
-    path: ${Directory.current.path}/flutter_web/packages/flutter_web_ui
-  over_react:
-    path: ${Directory.current.path}/flutter_web/packages/flutter_web_ui
-  web_skin:
-    path: ${Directory.current.path}/flutter_web/packages/flutter_web_ui
-  web_skin_dart:
-    path: ${Directory.current.path}/flutter_web/packages/flutter_web_ui
- */
