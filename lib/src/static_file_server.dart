0a31c2c7b49759166deef6dd0deac81cd596ad8f
Joseph Bingham <joe.bingham@workiva.com>
2019-09-26 11:28:59 -0600

    Clean up

--- lib/src/static_file_server.dart
+++ lib/src/static_file_server.dart
@@ -4,19 +4,19 @@ import 'package:rpc/common.dart';
 @ApiClass(name: 'staticfileserver', version: 'v1')
 class StaticFileServer {
 
-
-
   @ApiMethod(method: 'GET', path: 'getFolder/{sessionId}/{path}')
-  Future<Test> getFolder(/*SessionId*/ String sessionId, String path) {
+  Future<SessionFolder> getFolder(/*SessionId*/ String sessionId, String path) {
 
-    return Future.value(Test('The session ID is: ${sessionId}; The Path is: '
+    return Future.value(SessionFolder('The session ID is: ${sessionId}; The '
+        'Path '
+        'is: '
         '${path}'));
   }
 
 }
 
-class Test {
-  String peanutButter;
+class SessionFolder {
+  String path;
 
-  Test(this.peanutButter);
+  SessionFolder(this.path);
 }
