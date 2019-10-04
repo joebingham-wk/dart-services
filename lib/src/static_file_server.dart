570e0f2225c99c1fc745d85b1b497eb6b8b7c4ba
Greg Littlefield <greg.littlefield@workiva.com>
2019-10-04 11:37:20 -0700

    Patch compiled DDC files to work cross-origin

--- lib/src/static_file_server.dart
+++ lib/src/static_file_server.dart
@@ -21,16 +21,45 @@ class StaticFileServer {
     final outputFolderPath = p.canonicalize(project.outputDirectory.path);
     // Use `p.separator` instead of `p.join` so that `path` isn't treated as absolute
     // if it has a leading slash.
-    final outputFile = File(p.canonicalize(outputFolderPath + p.separator + path));
+    var outputFile = File(p.canonicalize(outputFolderPath + p.separator + path));
 
     if (!p.isWithin(outputFolderPath, outputFile.path)) {
       return Response.forbidden('"$path" is outside of output folder');
     }
 
-    if (!outputFile.existsSync()) {
+    var outputFileExists = outputFile.existsSync();
+    if (!outputFileExists && p.extension(outputFile.path) == '.bootstrap') {
+      outputFile = File('${outputFile.path}.js');
+      outputFileExists = outputFile.existsSync();
+    }
+    if (!outputFileExists) {
       return Response.notFound('"$path" not found within output files');
     }
 
-    return Response.ok(await outputFile.readAsString(), headers: {'content-type': lookupMimeType(path)});
+    return Response.ok((await outputFile.readAsString())
+        // main.bootstrap.dart.js
+        .replaceAll(
+          r'window.$dartLoader.rootDirectories.push(window.location.origin + baseUrl);',
+          r'window.$dartLoader.rootDirectories.push(baseUrl);'
+        )
+        // main.bootstrap.dart.js
+        .replaceAll(
+          r"var src = window.location.origin + '/' + modulePath + '.js';",
+          r"var src = baseUrl + '/' + modulePath + '.js';"
+        )
+        // main.dart.js
+        // main.bootstrap.dart.js
+        // more?
+        .replaceAll(
+          r'&& el[0].getAttribute' '\n'r'    ("href").startsWith("/")',
+          '',
+        )
+        // TODO is this needed?
+        // main.dart.js
+        .replaceAll(
+          r'var mainUri = _currentDirectory + "main.dart.bootstrap";',
+          r'var mainUri = "main.dart.bootstrap";'
+        )
+        , headers: {'content-type': lookupMimeType(outputFile.path)});
   }
 }
